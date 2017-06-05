aadc80ca79579c5b9b7f6a8a13e63d6bb0c8e991
Farhan Jameel Ahmed <farhanahmed1244@gmail.com>
2017-06-05 13:15:46 +0000

    add has_secure password add and bcrypt in gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,7 @@ class User < ActiveRecord::Base
                 uniqueness: {case_sensitive: false},
                 length: {maximum: 105},
                 format: {with: VALID_EMAIl_REGEX}
+                
+    has_secure_password
     
 end
